1caf4c91db169fa4b2240f6f3c0f25ae8e8d87f2
Richard Raya <rdxzv.dev@gmail.com>
2022-09-10 16:56:02 -0300

    nexus: Import from raven-user-13-TP1A.220624.021-8877034-release-keys

---
+++ config.mk
@@ -0,0 +1,10 @@
+# Permissions
+PRODUCT_PACKAGES += \
+    privapp-permissions-google-se
+
+# Packages
+PRODUCT_PACKAGES += \
+    PixelThemesStub \
+    PixelRecentsProvider \
+    NexusLauncherRelease \
+    WallpaperPickerGoogleRelease
